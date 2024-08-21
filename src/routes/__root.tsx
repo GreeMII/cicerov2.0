9e025281a49d3d7f2a96d26cd2626999a6e4373b
GreeMII <gleb_cry@mail.ru>
2024-08-21 19:45:11 +0700

    Almost end all adaptive html

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,21 @@
 import { useState } from 'react';
-import { Container, Group, Burger } from '@mantine/core';
+import {
+    HoverCard,
+    Group,
+    Button,
+    UnstyledButton,
+    SimpleGrid,
+    ThemeIcon,
+    Divider,
+    Center,
+    Box,
+    Burger,
+    Drawer,
+    Collapse,
+    ScrollArea,
+    rem,
+    useMantineTheme, Container,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Text, Anchor, Title } from '@mantine/core';
 import classes from './__root.module.css';
@@ -7,13 +23,15 @@ import {Link, Outlet, createFileRoute, createRootRouteWithContext} from "@tansta
 import {QueryClient} from "@tanstack/react-query";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import '@mantine/core/styles.css'
+import theme from "tailwindcss/defaultTheme";
+import {IconChevronDown} from "@tabler/icons-react";
 const links = [
     { link: '/', label: 'Главная' },
     { link: '/scenario', label: 'Конструктор' },
 ];
 
 const Header: React.FC = () => {
-    const [opened, { toggle }] = useDisclosure(false);
+    const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
     const [active, setActive] = useState(links[0].link);
 
     const items = links.map((link) => (
@@ -29,18 +47,34 @@ const Header: React.FC = () => {
     return (
         <header className={classes.header}>
             <Container size="2xl" className={classes.inner}>
-
-                <div className={classes.logo}><Title size="h1" >Cicero</Title></div>
-                <Group gap={2} visibleFrom="xs">
+                <div className={classes.logo}><Title size="h1">Cicero</Title></div>
+                <Group gap={2} visibleFrom="sm">
                     <div className={classes.links}>
                         {items}
                     </div>
                 </Group>
-
-                <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size=" sm"/>
+                <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm"/>
             </Container>
-            <Outlet />
-            <TanStackRouterDevtools />
+
+            <Drawer
+                opened={drawerOpened}
+                onClose={closeDrawer}
+                size="20%"
+                position="top"
+                padding="md"
+                title="Cicero"
+                hiddenFrom="sm"
+                className={classes.drawer}
+                color="#DEE2E6"
+            >
+                <Divider my="sm"/>
+                <div className={classes.smallbar}>
+                    {items}
+                </div>
+
+            </Drawer>
+            <Outlet/>
+            <TanStackRouterDevtools/>
         </header>
     );
 }
